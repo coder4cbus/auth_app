3364b94aa5a0504f9e379c0e69dd963d034781d6
coder4cbus <coder4cbus@gmail.com>
2018-03-07 10:23:02 -0500

    user reg with flash errors'

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+#shows the reg form
+def new
+  @user = User.new
+end
+
+# POST req to reg via new user
+def create
+  @user = User.new(user_params)
+
+  if @user.save
+    flash[:message] = "User successfully created!"
+    session[:user_id] = @user.id
+    redirect_to root
+  else
+    flash[:errors] = @user.errors.full_messages
+    render :new
+  end
+end
+
+#shows the edit form
+def edit
+end
+
+#POST req to update a user's info
+def update
+end
+
+
+def show
+  @user = User.find(params[:id])
+end
+
+def destroy
+  # destroy user
+end
+
+def user_params
+  params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'users#new' as 'new_user'
+
+  resources :users, only: [:index, :new, :create]
 end
+
+#show and edit will be one and the same
+
+# get     'users/new' =>          'users#new', as: 'new_user'
+#
+# get     'users' =>              'users#index', as: 'users'
+#
+# post    'users' =>              'users#create'
+#
+# get     'users/:id' =>          'users#show', as: 'user'
+#
+# patch   'users/:id' =>          'users#update'
+#
+# get     'users/:id/edit' =>     'users#edit', as: 'edit_user'
